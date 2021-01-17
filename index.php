bf76ed35f0592de257c80f031d08891ced288ee6
anthonymcsweeney1 <32956943+anthonymcsweeney1@users.noreply.github.com>
2021-01-17 14:46:17 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,61 @@
-<?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:117380531fyp.database.windows.net,1433; Database = FYP117380531", "fyp117380531", "FYP2021!");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    print("Suits them");
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
+<?php 
+session_start(); 
+
+
+include "db_conn.php";
+
+if (isset($_POST['uname']) && isset($_POST['password'])) {
+// validate user input in fields
+	function validate($data){
+       $data = trim($data);
+	   $data = stripslashes($data);
+	   $data = htmlspecialchars($data);
+	   return $data;
+	}
+
+	$uname = validate($_POST['uname']);
+	$pass = validate($_POST['password']);
+        
+
+// if field empty
+	if (empty($uname)) {
+		header("Location: index.php?error=Username is required");
+	    exit();
+	}else if(empty($pass)){
+        header("Location: index.php?error=Password is required");
+	    exit();
+	}else{
+		$sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+
+		$result = mysqli_query($conn, $sql);
+
+		if (mysqli_num_rows($result) === 1) {
+			$row = mysqli_fetch_assoc($result);
+            if ($row['user_name'] === $uname && $row['password'] === $pass) {
+              
+              
+// take user to page based on user type
+              switch($row['User_Type']) {
+      case 'Approver':
+        header("location: Approver.php");
+        break;
+      case 'User':
+        header("location: Home.php");
+        break;
+      default:
+              }
+		        exit();
+            }else{
+				header("Location: index.php?error=Incorect Username or password");
+		        exit();
+			}
+		}else{
+			header("Location: index.php?error=Incorect Username or password");
+	        exit();
+		}
+	}
+	
+}else{
+	header("Location: index.php");
+	exit();
 }
