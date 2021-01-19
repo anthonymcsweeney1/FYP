5ce74ff30e8d6d5afb48f470892e8c0a5a79a62f
anthonymcsweeney1 <32956943+anthonymcsweeney1@users.noreply.github.com>
2021-01-19 13:28:24 +0000

    Update login.php

--- login.php
+++ login.php
@@ -1,61 +1,33 @@
 <?php 
 session_start(); 
 
-
 include "db_conn.php";
 
-if (isset($_POST['uname']) && isset($_POST['password'])) {
 
 
-	$uname = trim($_POST['uname']);
-	$pass = trim($_POST['password']);
-
-
-// if field empty
-	if (empty($uname)) {
-		header("Location: index.php?error=Username is required");
-	    exit();
-	}else if(empty($pass)){
-        header("Location: index.php?error=Password is required");
-	    exit();
-	}else{
-		$query = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
-                $stmt = $conn->prepare($query);
-		
-	$stmt->bindParam('user_name', $uname, PDO::PARAM_STR);
-      $stmt->bindValue('password', $pass, PDO::PARAM_STR);
+  $username = trim($_POST['uname']);
+  $password = trim($_POST['password']);
+  if($username != "" && $password != "") {
+    try {
+      $query = "select * from `users` where `user_name`=:username and `password`=:password";
+      $stmt = $db->prepare($query);
+      $stmt->bindParam('user_name', $username, PDO::PARAM_STR);
+      $stmt->bindValue('password', $password, PDO::PARAM_STR);
       $stmt->execute();
       $count = $stmt->rowCount();
       $row   = $stmt->fetch(PDO::FETCH_ASSOC);
-      if($count == 1 && !empty($row)) 
-                // get session values
-
-
-// take user to page based on user type
-       switch($row['User_Type']) {
-      case 'OfferOwner':
-        header("location: test.php");
-        break;
-      case 'Processor':
-        header("location: test.php");
-        break;
-    case 'BFM':
-        header("location: test.php");
-        break;
-      default:
-              }
-		        exit();
-            }else{
-				header("Location: index.php?error=Incorect Username or password");
-		        exit();
-			}
-		}else{
-			header("Location: index.php?error=Incorect Username or password");
-	        exit();
-		}
-	}
-
-}else{
-	header("Location: index.php");
-	exit();
+      if($count == 1 && !empty($row)) {
+        /******************** Your code ***********************/
+      header("location: test.php");
+       
+      } else {
+        $msg = "Invalid username and password!";
+      }
+    } catch (PDOException $e) {
+      echo "Error : ".$e->getMessage();
+    }
+  } else {
+    $msg = "Both fields are required!";
+  }
 }
+?>
