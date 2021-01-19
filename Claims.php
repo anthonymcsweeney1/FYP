66bb76042b27843d888d445c83c7471b731abaac
anthonymcsweeney1 <32956943+anthonymcsweeney1@users.noreply.github.com>
2021-01-19 14:40:03 +0000

    Update Claims.php

--- Claims.php
+++ Claims.php
@@ -1,22 +1,4 @@
 
-<?php
-include 'functions.php';
-// Connect to MySQL database
-$pdo = pdo_connect_mysql();
-// Get the page via GET request (URL param: page), if non exists default the page to 1
-$page = isset($_GET['page']) && is_numeric($_GET['page']) ? (int)$_GET['page'] : 1;
-// Number of records to show on each page
-$records_per_page = 8;
-// Prepare the SQL statement and get records from our claims table, LIMIT will determine the page
-$stmt = $pdo->prepare("SELECT * FROM claims  ORDER BY Claimid LIMIT :current_page, :record_per_page");
-$stmt->bindValue(':current_page', ($page-1)*$records_per_page, PDO::PARAM_INT);
-$stmt->bindValue(':record_per_page', $records_per_page, PDO::PARAM_INT);
-$stmt->execute();
-// Fetch the records so we can display them in our template.
-$claims = $stmt->fetchAll(PDO::FETCH_ASSOC);
-// Get the total number of $claims, this is so we can determine whether there should be a next and previous button
-$num_claims = $pdo->query("SELECT COUNT(*) FROM claims")->fetchColumn();
-?>
 
 <style>
     
