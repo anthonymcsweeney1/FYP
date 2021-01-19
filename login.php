4a91894878646a354ee623bc3593eb56a6aa3591
anthonymcsweeney1 <32956943+anthonymcsweeney1@users.noreply.github.com>
2021-01-19 13:24:41 +0000

    Update login.php

--- login.php
+++ login.php
@@ -5,16 +5,10 @@ session_start();
 include "db_conn.php";
 
 if (isset($_POST['uname']) && isset($_POST['password'])) {
-// validate user input in fields
-	function validate($data){
-       $data = trim($data);
-	   $data = stripslashes($data);
-	   $data = htmlspecialchars($data);
-	   return $data;
-	}
 
-	$uname = validate($_POST['uname']);
-	$pass = validate($_POST['password']);
+
+	$uname = trim($_POST['uname']);
+	$pass = trim($_POST['password']);
 
 
 // if field empty
@@ -25,18 +19,20 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
         header("Location: index.php?error=Password is required");
 	    exit();
 	}else{
-		$sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
-
-		$result = mysqli_query($conn, $sql);
-
-		if (mysqli_num_rows($result) === 1) {
-			$row = mysqli_fetch_assoc($result);
-            if ($row['user_name'] === $uname && $row['password'] === $pass) {
+		$query = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+                $stmt = $conn->prepare($query);
+		
+	$stmt->bindParam('user_name', $uname, PDO::PARAM_STR);
+      $stmt->bindValue('password', $pass, PDO::PARAM_STR);
+      $stmt->execute();
+      $count = $stmt->rowCount();
+      $row   = $stmt->fetch(PDO::FETCH_ASSOC);
+      if($count == 1 && !empty($row)) 
                 // get session values
 
 
 // take user to page based on user type
-              switch($row['User_Type']) {
+       switch($row['User_Type']) {
       case 'OfferOwner':
         header("location: test.php");
         break;
