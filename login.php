a66534c76462a15092804e32564ef3a165e27566
anthonymcsweeney1 <32956943+anthonymcsweeney1@users.noreply.github.com>
2021-01-19 13:15:09 +0000

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,39 @@
+<?php 
+session_start();
+include("db_con.php");
+?>
+<?php
+$msg = ""; 
+if(isset($_POST['submitBtnLogin'])) {
+  $username = trim($_POST['username']);
+  $password = trim($_POST['password']);
+  if($username != "" && $password != "") {
+    try {
+      $query = "select * from `users` where `user_name`=:username and `password`=:password";
+      $stmt = $db->prepare($query);
+      $stmt->bindParam('username', $username, PDO::PARAM_STR);
+      $stmt->bindValue('password', $password, PDO::PARAM_STR);
+      $stmt->execute();
+      $count = $stmt->rowCount();
+      $row   = $stmt->fetch(PDO::FETCH_ASSOC);
+      if($count == 1 && !empty($row)) {
+        /******************** Your code ***********************/
+        $_SESSION['sess_user_id']   = $row['id'];
+        $_SESSION['sess_user_name'] = $row['user_name'];
+        $_SESSION['sess_name'] = $row['password'];
+       
+      } else {
+        $msg = "Invalid username and password!";
+      }
+    } catch (PDOException $e) {
+      echo "Error : ".$e->getMessage();
+    }
+  } else {
+    $msg = "Both fields are required!";
+  }
+}
+?>
+
 <form method="post">
   <table class="loginTable">
      <tr>
