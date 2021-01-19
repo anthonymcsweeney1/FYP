3d5b110c0aeaf96bcc416cfed50a3f2202b8036e
anthonymcsweeney1 <32956943+anthonymcsweeney1@users.noreply.github.com>
2021-01-19 13:59:44 +0000

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,76 @@
-
-<?php
+<?php  
+  
+ 
+ try  
+ {  
+    
+   $connect = new PDO("sqlsrv:server = tcp:117380531fyp.database.windows.net,1433; Database = FYP117380531", "fyp117380531", "FYP2021!");
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      if(isset($_POST["login"]))  
+      {  
+           if(empty($_POST["username"]) || empty($_POST["password"]))  
+           {  
+                $message = '<label>All fields are required</label>';  
+           }  
+           else  
+           {  
+                $query = "SELECT * FROM users WHERE username = :username AND password = :password";  
+                $statement = $connect->prepare($query);  
+                $statement->execute(  
+                     array(  
+                          'username'     =>     $_POST["username"],  
+                          'password'     =>     $_POST["password"]  
+                     )  
+                );  
+                $count = $statement->rowCount();  
+                if($count > 0)  
+                {  
+                     
+                     header("location:test.php");  
+                }  
+                else  
+                {  
+                     $message = '<label>Wrong Data</label>';  
+                }  
+           }  
+      }  
+ }  
+ catch(PDOException $error)  
+ {  
+      $message = $error->getMessage();  
+ }  
+ ?>  
+ <!DOCTYPE html>  
+ <html>  
+      <head>  
+           <title>Webslesson Tutorial | PHP Login Script using PDO</title>  
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+      </head>  
+      <body>  
+           <br />  
+           <div class="container" style="width:500px;">  
+                <?php  
+                if(isset($message))  
+                {  
+                     echo '<label class="text-danger">'.$message.'</label>';  
+                }  
+                ?>  
+                <h3 align="">PHP Login Script using PDO</h3><br />  
+                <form method="post">  
+                     <label>Username</label>  
+                     <input type="text" name="username" class="form-control" />  
+                     <br />  
+                     <label>Password</label>  
+                     <input type="password" name="password" class="form-control" />  
+                     <br />  
+                     <input type="submit" name="login" class="btn btn-info" value="Login" />  
+                </form>  
+           </div>  
+           <br />  
+      </body>  
+   <?php
 echo "<table style='border: solid 1px black;'>";
 echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
 
@@ -26,7 +97,7 @@ class TableRows extends RecursiveIteratorIterator {
 try {
  $conn = new PDO("sqlsrv:server = tcp:117380531fyp.database.windows.net,1433; Database = FYP117380531", "fyp117380531", "FYP2021!");
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $stmt = $conn->prepare("SELECT id, user_name, password FROM users");
+  $stmt = $conn->prepare("SELECT id, username, password FROM users");
   $stmt->execute();
 
   // set the resulting array to associative
@@ -40,3 +111,4 @@ try {
 $conn = null;
 echo "</table>";
 ?>
+ </html>  
